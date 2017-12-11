a1b9f00e9c6885f3fa70e47b16e06c2fc5427751
StefanSebastian <stefansebii@gmail.com>
2017-12-11 22:28:02 +0200

    Added a message on login fail

--- AndroidAppReactive/app/src/main/java/com/example/sebi/androidappreactive/LoginActivity.java
+++ AndroidAppReactive/app/src/main/java/com/example/sebi/androidappreactive/LoginActivity.java
@@ -7,9 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sebi.androidappreactive.model.User;
 import com.example.sebi.androidappreactive.net.UserResourceClient;
+import com.jakewharton.retrofit2.adapter.rxjava2.HttpException;
+
+import org.json.JSONObject;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -52,6 +56,21 @@ public class LoginActivity extends AppCompatActivity {
         mDisposable = mUserResourceClient.login$(user)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(token -> Log.d(TAG, token.toString()));
+                .subscribe(token -> mRealm.executeTransactionAsync(
+                        realm -> {
+                            user.setToken(token.getToken());
+                            realm.copyToRealmOrUpdate(user);
+                        }
+                    ),
+                        error -> {
+                            String msg = "authentication error";
+                            if (error instanceof HttpException){
+                                HttpException e = (HttpException) error;
+                                msg = e.response().errorBody().string();
+                            }
+                            Log.e(TAG, "error authenticating", error);
+                            Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+                            toast.show();}
+                    );
     }
 }
