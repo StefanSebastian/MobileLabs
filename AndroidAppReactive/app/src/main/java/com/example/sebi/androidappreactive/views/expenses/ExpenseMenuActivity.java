da249be38f293f34db47af294a3239e8ce6fbf29
StefanSebastian <stefansebii@gmail.com>
2017-12-29 13:06:24 +0200

    Added an extra menu animation

--- AndroidAppReactive/app/src/main/java/com/example/sebi/androidappreactive/views/expenses/ExpenseMenuActivity.java
+++ AndroidAppReactive/app/src/main/java/com/example/sebi/androidappreactive/views/expenses/ExpenseMenuActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import com.example.sebi.androidappreactive.R;
@@ -17,18 +19,31 @@ Simple menu activity
  */
 public class ExpenseMenuActivity extends AppCompatActivity {
 
+    private Button mAddExpenseButton;
+    private Button mListExpensesButton;
+    private Button mChartExpensesButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_menu);
 
-        Button addExpenseButton = findViewById(R.id.expenseAddButton);
-        Button listExpensesButton = findViewById(R.id.expenseListButton);
-        Button chartExpensesButton = findViewById(R.id.expenseChartButton);
+        mAddExpenseButton = findViewById(R.id.expenseAddButton);
+        mListExpensesButton = findViewById(R.id.expenseListButton);
+        mChartExpensesButton = findViewById(R.id.expenseChartButton);
 
-        addExpenseButton.setOnClickListener(v -> openAddExpenseView());
-        listExpensesButton.setOnClickListener(v -> openListExpensesView());
-        chartExpensesButton.setOnClickListener(v -> openChartExpensesView());
+        mAddExpenseButton.setOnClickListener(v -> openAddExpenseView());
+        mListExpensesButton.setOnClickListener(v -> openListExpensesView());
+        mChartExpensesButton.setOnClickListener(v -> openChartExpensesView());
+
+        animateMenu();
+    }
+
+    private void animateMenu(){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.expense_menu_animation);
+        mAddExpenseButton.startAnimation(animation);
+        mListExpensesButton.startAnimation(animation);
+        mChartExpensesButton.startAnimation(animation);
     }
 
     private void openAddExpenseView(){
